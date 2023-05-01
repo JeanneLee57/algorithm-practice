4fac057a2c27c9a917c800c175b1eca6fce8afcb
JeanneLee57 <122351417+JeanneLee57@users.noreply.github.com>
2023-05-01 10:43:11 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 25.49 ms, Memory: 41.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -0,0 +1,28 @@
+function solution(n, wires) {
+    const graph = Array.from({length: n + 1}, () => Array(n + 1).fill(0))
+    const dfs = (arr, node) => {
+        let res = 0
+        for (let i = 1; i <= arr[node].length; i += 1) {
+            if (arr[node][i]) { // 연결되어 있다면 방문 처리
+                arr[node][i] = arr[i][node] = 0
+                res += 1 + dfs(arr, i)
+            } 
+        }
+        return res
+    }
+    let result = 100
+    
+    // 와이어 연결 정보 저장
+    wires.forEach(wire => {
+        graph[wire[0]][wire[1]] = graph[wire[1]][wire[0]] = 1
+    })
+    console.log(graph)
+    // 와이어를 하나씩 끊고 끊어진 각 노드 2개에 대하여 DFS수행 후의 차이를 계산
+    wires.forEach(wire => {
+        const copy = graph.map(v => v.slice());
+        const [a, b] = wire
+        copy[a][b] = copy[b][a] = 0
+        result = Math.min(result, Math.abs(dfs(copy, a) - dfs(copy, b)))
+    })
+    return result
+}
