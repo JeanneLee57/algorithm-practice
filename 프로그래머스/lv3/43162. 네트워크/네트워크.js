c5ad83561f545ce87a01e1f8e0f8dc58e74733aa
JeanneLee57 <122351417+JeanneLee57@users.noreply.github.com>
2023-09-05 16:16:15 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.js
@@ -0,0 +1,20 @@
+function solution(n, computers) {
+  const visited = Array(n).fill(false);
+  let answer = 0;
+
+  function dfs(node) {
+    visited[node] = true;
+    for (let i = 0; i < n; i++) {
+      if (computers[node][i] && !visited[i]) {
+        dfs(i);
+      }
+    }
+  }
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      answer++;
+    }
+  }
+  return answer;
+}
