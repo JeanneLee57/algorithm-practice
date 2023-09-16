1bc17fbaea2099f23b70bf3c000904e549096aff
JeanneLee57 <122351417+JeanneLee57@users.noreply.github.com>
2023-09-17 00:56:33 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.57 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,24 +1,23 @@
 function solution(board) {
     const l = board.length
-    const dir = [[1,0], [-1,0], [0, 1],  [0, -1]]
-    const visited = Array(l).fill().map(() => Array(l).fill().map(() => Array(4).fill(Infinity)));
+    const dir = [[1,0], [0, 1], [-1,0], [0, -1]]
     const bfs = (sx, sy) => {
         const q = []
-        q.push([sx, sy, -1, 0])
+        q.push([sx, sy, -1])
         while(q[0]){
-            const [cx, cy, cd, cost] = q.shift()
+            const [cx, cy, cd] = q.shift()
             for(let i=0; i<4; i++){
                 const nx = cx + dir[i][0]
                 const ny = cy + dir[i][1]
-                if(nx<0 || ny<0 || nx>=l || ny>=l || board[nx][ny] === 1) continue
-                const newCost = (cd === -1 || cd === i) ? cost + 100 : cost + 600
-                if(visited[nx][ny][i] > newCost){
-                    visited[nx][ny][i] = newCost
-                    q.push([nx, ny, i, newCost])                    
+                if(nx<0 || ny<0 || nx>=l || ny>=l) continue
+                const cost = (cd === -1 || cd === i) ? 100 : 600
+                if(board[nx][ny] === 0 || board[nx][ny] >= board[cx][cy] + cost){
+                    board[nx][ny] = board[cx][cy] + cost
+                    q.push([nx, ny, i])                    
                 }
             }
         }
     }
     bfs(0,0)
-    return Math.min(...visited[l-1][l-1])
+    return board[l-1][l-1]
 }
